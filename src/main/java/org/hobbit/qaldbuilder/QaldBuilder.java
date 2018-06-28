217548edeca9ca15d6f3512e65ed2d4f3dc270e5
Abdelgadir Awadelkareem Hassan <mabdelgadi@iais.fraunhofer.de>
2018-06-28 11:06:02 +0200

    version 2

--- src/main/java/org/hobbit/qaldbuilder/QaldBuilder.java
+++ src/main/java/org/hobbit/qaldbuilder/QaldBuilder.java
@@ -5,15 +5,22 @@ import java.io.ByteArrayOutputStream;
 import org.apache.jena.atlas.json.JSON;
 import org.apache.jena.atlas.json.JsonBuilder;
 import org.apache.jena.atlas.json.JsonObject;
+import org.apache.jena.atlas.json.JsonValue;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.ResultSetFormatter;
-
+/**
+ * 
+ * @author Mohammed Abdelgadir, github.com/weekmo
+ *
+ */
 public class QaldBuilder {
 	JsonObject qaldFormat,questionObject;
 	JsonBuilder jsonBuilder;
 	String query;
-	
+	/**
+	 * Constructor adds empty dataset object and questions array.
+	 */
 	public QaldBuilder() {
 		jsonBuilder = new JsonBuilder();
 		jsonBuilder.startObject()
@@ -25,29 +32,38 @@ public class QaldBuilder {
 		this.questionObject = this.qaldFormat.get("questions").getAsArray().get(0).getAsObject();
 	}
 	
+	/**
+	 * Set current qald question as a start
+	 * @param question: in json format as a String
+	 */
 	public void setQuestionAsJson(String question) {
 		this.questionObject=JSON.parse(question);
-		if(this.questionObject.hasKey("_id"))
-			this.questionObject.remove("_id");
+		this.remove("_id");
 		
 		this.setQuery(this.questionObject.get("query").getAsObject().get("sparql").toString());
 	}
 	
 	public void setQuery(String query) {
 		query = query.trim().replace("\"", "");
-		if(this.questionObject.hasKey("query"))
-			this.questionObject.remove("query");
+		this.removeQuery();
 		this.questionObject.put("query", query);
 		this.query=query;
 	}
 	
+	/**
+	 * Set answer as json object
+	 * @param answers
+	 */
 	public void setAnswers(JsonObject answers) {
-		if(this.questionObject.hasKey("answers"))
-			this.questionObject.remove("answers");
+		this.removeAnswers();
 		jsonBuilder.startArray().value(answers).finishArray();
 		this.questionObject.put("answers", jsonBuilder.build());
 	}
 	
+	/**
+	 * Set answer(s) by retrieving info from sparql service
+	 * @param sparqlService
+	 */
 	public void setAnswers(String sparqlService) {
 		if(this.questionObject.hasKey("answers"))
 			this.questionObject.remove("answers");
@@ -64,6 +80,11 @@ public class QaldBuilder {
 		this.questionObject.put("answers", JSON.parse(outputStream.toString()));
 	}
 	
+	/**
+	 * Set question string and language
+	 * @param question
+	 * @param language
+	 */
 	public void setQuestionString(String question,String language) {
 		language=language.trim().replace("\"", "");
 		if(this.questionObject.hasKey("question"))
@@ -93,18 +114,62 @@ public class QaldBuilder {
 		this.questionObject.put("aggregation", value);
 	}
 	
+	/**
+	 * Set question ID
+	 * @param value
+	 */
 	public void setID(int value) {
 		if(this.questionObject.hasKey("id"))
 			this.questionObject.remove("id");
 		this.questionObject.put("id", value);
 	}
 	
+	/**
+	 * Set Dataset ID
+	 * @param id
+	 */
 	public void setDatasetID(String id) {
 		if(this.qaldFormat.get("dataset").getAsObject().hasKey("id"))
 			this.qaldFormat.get("dataset").getAsObject().remove("id");
 		this.qaldFormat.get("dataset").getAsObject().put("id", id);
 	}
 	
+	public void removeQuery() {
+		if(this.questionObject.hasKey("query"))
+			this.questionObject.remove("query");
+	}
+	
+	public void removeAnswers() {
+		if(this.questionObject.hasKey("answers"))
+			this.questionObject.remove("answers");
+	}
+	
+	public void remove(String key) {
+		if(this.questionObject.hasKey(key))
+			this.questionObject.remove(key);
+	}
+	
+	public void add(String key,JsonValue value) {
+		this.questionObject.put(key, value);
+	}
+	
+	public void add(String key,String value) {
+		this.questionObject.put(key, value);
+	}
+	
+	public void add(String key,long value) {
+		this.questionObject.put(key, value);
+	}
+	
+	public void add(String key,boolean value) {
+		this.questionObject.put(key, value);
+	}
+	
+	
+	/**
+	 * Get all question info as Qald format
+	 * @return
+	 */
 	public String getQaldQuestion() {
 		this.qaldFormat.get("questions").getAsArray().clear();
 		this.qaldFormat.get("questions").getAsArray().add(this.questionObject);
